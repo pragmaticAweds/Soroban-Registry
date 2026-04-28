f92e7e368f745cf304e993553cbde3e405e25402
ALIPHATIC D. ‎فؤاد <105937740+ALIPHATICHYD@users.noreply.github.com>
2026-04-28 23:41:11 +0100

    fix: replace deprecated getDefaultMiddleware with getDefaultMiddleware callback pattern
    - getDefaultMiddleware was removed from @reduxjs/toolkit v2+
    - Replace with middleware callback in configureStore
    - Deduplicate persist/PAUSE in ignoredActions list

--- frontend/store/index.ts
+++ frontend/store/index.ts
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
@@ -15,25 +15,24 @@ const persistConfig = {
   key: "root",
   version: 1,
   storage,
-  // blacklist: ['someTransientSlice'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [
-        "persist/PERSIST",
-        "persist/REHYDRATE",
-        "persist/FLUSH",
-        "persist/PAUSE",
-        "persist/REGISTER",
-        "persist/PAUSE",
-      ],
-    },
-  }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          "persist/PERSIST",
+          "persist/REHYDRATE",
+          "persist/FLUSH",
+          "persist/PAUSE",
+          "persist/REGISTER",
+        ],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
